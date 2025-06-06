5305e8a3bbf896535aa36f82e5a185d85116fc83
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-06 23:39:26 +0300

    feat: add about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,31 @@
-import "About.css";
+import "../about/About.css";
+import Mohammed from "/src/assets/images/mohammed.webp";
 
 function About() {
-  return <section></section>;
+  return (
+    <section className="about" id="about">
+      <div className="profile-image">
+        <div className="image-wrapper">
+          <img className="profile-picture" src={Mohammed} alt="mohammed abdi" />
+        </div>
+        <span className="location">
+          <i class="bx bxs-map"></i>
+          Dire Dawa, 3000 Ethiopia
+        </span>
+      </div>
+      <div className="about-me">
+        <span className="about-title">A bit about myself</span>
+        <div className="about-description">
+          Hi, I’m Mohammed Abdi a passionate front-end engineer who loves
+          building smooth, interactive web experiences. Whether it’s clean UIs
+          with React.js, elegant Vanilla JavaScript, or diving into C++ logic, I
+          enjoy solving tough problems with simple, efficient code. From
+          frontend magic to backend logic, I thrive on learning, improving, and
+          turning ideas into reality.
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import "../layouts/MainLayout.css";
 import Header from "../components/header/Header.jsx";
 import Hero from "../components/hero/Hero.jsx";
+import About from "../components/about/About.jsx";
 
 function MainLayout() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
