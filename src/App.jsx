474a4e4a0c7d166f90b57f41bcc6c23b924de636
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-06 13:24:29 +0300

    feat: add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Background from "./components/background/Background";
 import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  return <MainLayout />;
+  return (
+    <>
+      <MainLayout />
+      <Background />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/background/Background.jsx
@@ -0,0 +1,7 @@
+import "../background/Background.css";
+
+function Background() {
+  return <div className="glow"></div>;
+}
+
+export default Background;
