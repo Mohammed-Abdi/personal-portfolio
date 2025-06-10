4917e953e89559bcca18bcdb5126c276b69b4777
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-10 21:31:21 +0300

    style: update the about section

--- src/layout/about/About.jsx
+++ src/layout/about/About.jsx
@@ -1,10 +1,31 @@
 import "./About.css";
 import socials from "../../data/socials.js";
 import Link from "../../components/link/Link.jsx";
+import { Icon } from "@iconify/react";
 
 function About() {
   return (
     <section className="about">
+      <div className="about-me">
+        <div className="image-wrapper">
+          <Icon icon="lets-icons:user" className="icon" />
+          <img
+            src="image/profile/mohammed-abdi.webp"
+            alt="profile picture"
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className="personal-info-wrapper">
+          <span className="name">Mohammed Abdi</span>
+          <span className="profession">Frontend Engineer</span>
+          <span className="location">
+            <Icon icon="weui:location-filled" />
+            Dire Dawa, Ethiopia
+          </span>
+        </div>
+      </div>
       <div className="social-links">
         {socials.map((social, index) => {
           return (
