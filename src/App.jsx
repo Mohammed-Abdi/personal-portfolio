72b0be1722cbf06f9bafe31f63b1b6f169524aab
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-11 00:15:25 +0300

    feat: add a descriptive intro

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <Header />
       <main>
         <About />
+
         <Skills />
       </main>
     </>
--- src/layout/about/About.jsx
+++ src/layout/about/About.jsx
@@ -5,42 +5,48 @@ import { Icon } from "@iconify/react";
 
 function About() {
   return (
-    <section className="about">
-      <div className="my about-me">
-        <div className="image-wrapper">
-          <Icon icon="lets-icons:user" className="icon" />
-          <img
-            src="image/profile/mohammed-abdi.webp"
-            alt="profile picture"
-            loading="lazy"
-            width={50}
-            height={50}
-          />
-        </div>
-        <div className="personal-info-wrapper">
-          <span className="name">Mohammed Abdi</span>
-          <span className="profession">Frontend Engineer</span>
-          <span className="location">
-            <Icon icon="weui:location-filled" />
-            Dire Dawa, Ethiopia
-          </span>
-        </div>
-      </div>
-      <div className="my social-links">
-        <span className="link-title">Feel free to reach out!</span>
-        {socials.map((social, index) => {
-          return (
-            <Link
-              key={index}
-              name={social.name}
-              icon={social.icon}
-              link={social.link}
-              username={social.username}
+    <>
+      <section className="about">
+        <div className="my about-me">
+          <div className="image-wrapper">
+            <Icon icon="lets-icons:user" className="icon" />
+            <img
+              src="image/profile/mohammed-abdi.webp"
+              alt="profile picture"
+              loading="lazy"
+              width={50}
+              height={50}
             />
-          );
-        })}
-      </div>
-    </section>
+          </div>
+          <div className="personal-info-wrapper">
+            <span className="name">Mohammed Abdi</span>
+            <span className="profession">Frontend Engineer</span>
+            <span className="location">
+              <Icon icon="weui:location-filled" />
+              Dire Dawa, Ethiopia
+            </span>
+          </div>
+        </div>
+        <div className="my social-links">
+          <span className="link-title">Feel free to reach out!</span>
+          {socials.map((social, index) => {
+            return (
+              <Link
+                key={index}
+                name={social.name}
+                icon={social.icon}
+                link={social.link}
+                username={social.username}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <p className="intro">
+        I'm a Frontend Engineer with a passion for crafting beautiful,
+        responsive, and high-performance web applications
+      </p>
+    </>
   );
 }
 
