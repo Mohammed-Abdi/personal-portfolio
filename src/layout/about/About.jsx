0693b811f53af533fb76401db5fa92e3be73c2ef
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-10 19:55:26 +0300

    feat: add social links in about section

--- src/layout/about/About.jsx
+++ src/layout/about/About.jsx
@@ -1,7 +1,25 @@
 import "./About.css";
+import socials from "../../data/socials.js";
+import Link from "../../components/link/Link.jsx";
 
 function About() {
-  return <section className="about"></section>;
+  return (
+    <section className="about">
+      <div className="social-links">
+        {socials.map((social, index) => {
+          return (
+            <Link
+              key={index}
+              name={social.name}
+              icon={social.icon}
+              link={social.link}
+              username={social.username}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default About;
