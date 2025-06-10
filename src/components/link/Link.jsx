9b2e09e5f6a4e5e6a4459c7c8d6299e392980bdd
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-10 20:05:04 +0300

    fix: add return statement for it to return the links

--- src/components/link/Link.jsx
+++ src/components/link/Link.jsx
@@ -1,13 +1,16 @@
+import { Icon } from "@iconify/react";
 import "./Link.css";
 
 function Link(props) {
-  <a href={props} className="social-link">
-    <Icon icon={props.icon} className="social-link-icon" />
-    <div className="social-link-detail-wrapper">
-      <span className="social-media-name">{props.name}</span>
-      <span className="username">{props.username}</span>
-    </div>
-  </a>;
+  return (
+    <a href={props} className="social-link">
+      <Icon icon={props.icon} className="social-link-icon" />
+      <div className="social-link-detail-wrapper">
+        <span className="social-media-name">{props.name}</span>
+        <span className="username">{props.username}</span>
+      </div>
+    </a>
+  );
 }
 
 export default Link;
