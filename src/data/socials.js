ce50034bf200282e9bdc05446e0c92c09243256f
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-10 19:13:01 +0300

    feat: add social media links

---
+++ src/data/socials.js
@@ -0,0 +1,40 @@
+const socials = [
+  {
+    name: "Email",
+    icon: "ic:outline-email",
+    link: "mailto:mohammedabdi.ta@gmail.com",
+    username: "mohammed",
+  },
+  {
+    name: "Telegram",
+    icon: "ri:telegram-2-line",
+    link: "https://t.me/its_mamme",
+    username: "mohammed",
+  },
+  {
+    name: "Facebook",
+    icon: "uil:facebook",
+    link: "https://web.facebook.com/mohammed.abdi.71619533",
+    username: "mohammed",
+  },
+  {
+    name: "GitHub",
+    icon: "grommet-icons:github",
+    link: "https://github.com/Mohammed-Abdi",
+    username: "mohammed",
+  },
+  {
+    name: "Instagram",
+    icon: "mdi:instagram",
+    link: "https://www.instagram.com/its_mamme/",
+    username: "mohammed",
+  },
+  {
+    name: "Linkedin",
+    icon: "mdi:linkedin",
+    link: "https://www.linkedin.com/in/mohammed-abdi-tahir",
+    username: "mohammed",
+  },
+];
+
+export default socials;
