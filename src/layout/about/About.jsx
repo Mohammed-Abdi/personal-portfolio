bd78a8a49c37e175ffc04f249b14a1242e965873
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-11 17:33:00 +0300

    fix: removed image lazy loading for the profile picture

--- src/layout/about/About.jsx
+++ src/layout/about/About.jsx
@@ -13,7 +13,6 @@ function About() {
             <img
               src="image/profile/mohammed-abdi.webp"
               alt="profile picture"
-              loading="lazy"
               width={50}
               height={50}
             />
@@ -26,6 +25,9 @@ function About() {
               Dire Dawa, Ethiopia
             </span>
           </div>
+          <a className="download-cv" download href="docs">
+            Resume <Icon icon="tabler:download" />
+          </a>
         </div>
         <div className="my social-links">
           <span className="link-title">Feel free to reach out!</span>
