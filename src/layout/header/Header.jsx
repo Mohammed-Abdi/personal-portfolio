53f3cd74d956375daf415db8f941f9035fed8823
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-11 16:27:49 +0300

    refactor: removed unused properties

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -2,11 +2,7 @@ import ThemeToggle from "../../components/theme/ThemeButton.jsx";
 import "./Header.css";
 
 function Header() {
-  return (
-    <header>
-      <ThemeToggle />
-    </header>
-  );
+  return <header>{/* <ThemeToggle /> */}</header>;
 }
 
 export default Header;
