67687066dd981a31b5eb939e8bb8806a44c63b6b
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-12 07:51:02 +0300

    fix: remove unnecessary links from tech-stack

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -2,25 +2,21 @@ const techStack = [
   {
     name: "React.js",
     icon: "mdi:react",
-    link: "https://react.dev",
     rate: 60,
   },
   {
     name: "CSS",
     icon: "mdi:language-css3",
-    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     rate: 92,
   },
   {
     name: "Vanilla.js",
     icon: "ant-design:java-script-outlined",
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     rate: 87,
   },
   {
     name: "C++",
     icon: "nonicons:c-plusplus-16",
-    link: "https://cplusplus.com",
     rate: 75,
   },
 ];
