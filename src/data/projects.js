27d041e97ba395b61f75f73470ac32b023330b75
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-12 11:29:57 +0300

    feat: add repository link for each project

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,7 @@ const projects = [
       "A responsive portfolio built with React and CSS Modules, featuring clean design, smooth animations, and reusable components.",
     techStack: ["React.js", "Vanilla.js", "CSS"],
     link: "https://mohammedabdi.vercel.app",
+    repo: "https://github.com/Mohammed-Abdi/personal-portfolio",
   },
   {
     img: "preview/recipe.webp",
@@ -14,6 +15,7 @@ const projects = [
       "A simple recipe app using HTML, CSS, and JavaScript with an intuitive UI and interactive features for easy exploration.",
     techStack: ["Vanilla.js", "CSS", "HTML"],
     link: "https://recipes-recipes.vercel.app",
+    repo: "https://github.com/Mohammed-Abdi/Recipes",
   },
 ];
 
