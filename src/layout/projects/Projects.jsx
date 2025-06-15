0cec79f2a5cdcb325e5973dbd96f4fc8935847be
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-15 07:57:48 +0300

    feat: implement search functionality for filtering projects

--- src/layout/projects/Projects.jsx
+++ src/layout/projects/Projects.jsx
@@ -2,32 +2,45 @@ import "./Projects.css";
 import projects from "../../data/projects.js";
 import Project from "../../components/project/Project.jsx";
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 function Projects() {
+  const [search, setSearch] = useState("");
   return (
     <section className="projects">
       <span className="section-title">FEATURED PROJECTS</span>
       <div className="search">
-        <input type="text" id="search" name="search" placeholder="Search..." />
+        <input
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <button className="filter">
           <Icon icon="ep:filter" className="filter-icon" />
           Filter
         </button>
       </div>
       <div className="projects-wrapper">
-        {projects.map((project, index) => {
-          return (
-            <Project
-              key={index}
-              image={project.img}
-              title={project.title}
-              description={project.description}
-              link={project.link}
-              repo={project.repo}
-              techStack={project.techStack}
-            />
-          );
-        })}
+        {projects
+          .filter((project) =>
+            project.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((project, index) => {
+            return (
+              <Project
+                key={index}
+                image={project.img}
+                title={project.title}
+                description={project.description}
+                link={project.link}
+                repo={project.repo}
+                techStack={project.techStack}
+              />
+            );
+          })}
       </div>
       <a
         href="https://github.com/Mohammed-Abdi?tab=repositories"
