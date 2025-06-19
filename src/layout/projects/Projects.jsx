07852d650b40e59f8b0bba8f758b87e172aa8919
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-19 21:49:38 +0300

    style: update drop-down icon in projects filter

--- src/layout/projects/Projects.jsx
+++ src/layout/projects/Projects.jsx
@@ -29,7 +29,7 @@ function Projects() {
         />
         <div className="filter-button" onClick={toggleDropDown}>
           {year}
-          <Icon icon="fe:drop-down" />
+          <Icon icon="ri:arrow-down-s-line" />
           <div className={`years ${dropDown ? "dropped" : ""}`}>
             <button
               value={2025}
