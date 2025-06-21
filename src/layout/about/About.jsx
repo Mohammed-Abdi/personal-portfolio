0c1b5c880da921c8c639e6c1df8c7f7d5dd479d4
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-21 11:37:02 +0300

    refactor: replace icon imports with lazy loading for improved performance

--- src/layout/about/About.jsx
+++ src/layout/about/About.jsx
@@ -1,22 +1,28 @@
 import "./About.css";
 import socials from "../../data/socials.js";
 import Link from "../../components/link/Link.jsx";
-import iconMap from "../../data/iconMap.js";
+import { Suspense, lazy } from "react";
+
+const Verified = lazy(() => import("../../assets/icons/symbols/Verified.jsx"));
+const User = lazy(() => import("../../assets/icons/symbols/User.jsx"));
 
 function About() {
-  const Verified = iconMap["Verified"];
-  const User = iconMap["User"];
   return (
     <>
       <section className="about">
         <div className="my about-me hidden">
           <div className="image-wrapper">
-            <User className="icon" />
+            <Suspense fallback={<span>Loading icon...</span>}>
+              <User className="icon" />
+            </Suspense>
             <img
-              src="image/profile/mohammed-abdi.webp"
-              alt="mohammed abdi's profile picture"
+              src="/image/profile/mohammed-abdi.webp"
+              alt="Mohammed Abdi's profile picture"
               width={50}
               height={50}
+              decoding="async"
+              loading="eager" // Profile image likely above-the-fold
+              style={{ aspectRatio: "1/1" }} // maintain aspect ratio to avoid layout shifts
             />
           </div>
           <div className="personal-info-wrapper">
@@ -24,19 +30,17 @@ function About() {
             <span className="profession">Frontend Engineer</span>
             <span className="location">Based in Dire Dawa, Ethiopia</span>
           </div>
-          <Verified className="legit" />
+          <Suspense fallback={<span>Loading icon...</span>}>
+            <Verified className="legit" />
+          </Suspense>
         </div>
         <div className="my social-links hidden">
           <span className="link-title">Feel free to reach out!</span>
-          {socials.map((social, index) => {
-            return <Link key={index} name={social.name} link={social.link} />;
-          })}
+          {socials.map((social, index) => (
+            <Link key={index} name={social.name} link={social.link} />
+          ))}
         </div>
       </section>
-      {/* <p className="intro">
-        I'm a Frontend Engineer with a passion for crafting beautiful,
-        responsive, and high-performance web applications
-      </p> */}
     </>
   );
 }
