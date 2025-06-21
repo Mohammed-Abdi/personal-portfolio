8ea86da1c4ff04483e686e4bc6183d760d8b183d
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-21 12:17:19 +0300

    test: temporarily disable scroll animations for LCP testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import About from "./layout/about/About.jsx";
 import Education from "./layout/education/Education.jsx";
 import Experience from "./layout/Experience/Experience.jsx";
@@ -8,28 +8,28 @@ import Projects from "./layout/projects/Projects.jsx";
 import Skills from "./layout/skills/Skills.jsx";
 
 function App() {
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-          } else {
-            entry.target.classList.remove("show");
-          }
-        });
-      },
-      {
-        threshold: 0.01,
-      }
-    );
-    const hiddenElements = document.querySelectorAll(".hidden");
-    hiddenElements.forEach((el) => observer.observe(el));
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           entry.target.classList.add("show");
+  //         } else {
+  //           entry.target.classList.remove("show");
+  //         }
+  //       });
+  //     },
+  //     {
+  //       threshold: 0.01,
+  //     }
+  //   );
+  //   const hiddenElements = document.querySelectorAll(".hidden");
+  //   hiddenElements.forEach((el) => observer.observe(el));
 
-    return () => {
-      hiddenElements.forEach((el) => observer.unobserve(el));
-    };
-  }, []);
+  //   return () => {
+  //     hiddenElements.forEach((el) => observer.unobserve(el));
+  //   };
+  // }, []);
   return (
     <>
       <Header />
