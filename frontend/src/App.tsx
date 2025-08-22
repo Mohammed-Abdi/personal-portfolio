ebdf9671def6eda372f0199b41178461292587ea
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-08-22 10:19:36 +0300

    feat(nav): implement NavBar with Redux active state and glow - Added NavBar component displaying links from settings slice - Active link shows blurred glow using CSS variables - Click on a link dispatches setActive to update Redux state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import NavBar from "./components/layouts/NavBar";
+
 const App = () => {
-  return <h1>Test</h1>;
+  return <NavBar />;
 };
 
 export default App;
---
+++ frontend/src/components/layouts/NavBar.tsx
@@ -0,0 +1,51 @@
+import type React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../store/store";
+import { setActive } from "../../features/settings/settingsSlice";
+
+const NavBar: React.FC = () => {
+  const navLinks = useSelector((state: RootState) => state.settings.navLinks);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <nav
+      className="flex fixed top-5 left-1/2 -translate-x-1/2 rounded-full items-center gap-1.5 p-2 outline outline-[var(--color-border)] text-sm"
+      style={{
+        background:
+          "linear-gradient(var(--color-bg-overlay-20), var(--color-bg-overlay-10))",
+      }}
+    >
+      {navLinks.map((navLink) => (
+        <a
+          key={navLink.link}
+          href={navLink.link}
+          rel="noreferrer noopener"
+          onClick={() => dispatch(setActive(navLink.link))}
+          className={`relative flex items-center justify-center w-16 py-1.5 rounded-full transition-all duration-300
+        ${
+          navLink.isActive
+            ? "bg-[linear-gradient(var(--color-bg-overlay-20),var(--color-bg-overlay-10))]"
+            : ""
+        }`}
+        >
+          {navLink.label}
+
+          {navLink.isActive && (
+            <>
+              <div className="absolute -top-2.5 w-4 h-0.5 bg-amber-50 rounded-full"></div>
+              <div
+                className="absolute -top-2.5 w-6 h-2 opacity-50"
+                style={{
+                  background: "var(--color-text-primary)",
+                  filter: "blur(8px)",
+                }}
+              ></div>
+            </>
+          )}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default NavBar;
