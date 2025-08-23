3ebd34931a507c9ecd4cb999cb902d547ecf69b4
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-08-23 11:15:21 +0300

    fix(header): bring header to front with z-index - Added `z-[99999]` to header for proper stacking above other content - Ensures fixed header does not get hidden behind other elements

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
 const Header: React.FC<HeaderProps> = ({ children, ...props }) => {
   return (
     <>
-      <header className="flex items-center justify-center fixed top-0 w-full">
+      <header className="flex items-center justify-center fixed top-0 w-full z-[99999]">
         <div
           className="min-h-16 p-5 justify-between items-center w-full max-w-7xl"
           {...props}
