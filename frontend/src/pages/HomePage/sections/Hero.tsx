2e4a83a5a50096c64c4a6babf22ea559c956c852
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-08-23 11:20:32 +0300

    style(hero): adjust typography and muted text - Reduced heading size on small screens to `text-2xl` for better responsiveness - Added `text-sm md:text-base text-[var(--color-text-muted)]` to paragraph for improved readability and visual hierarchy

--- frontend/src/pages/HomePage/sections/Hero.tsx
+++ frontend/src/pages/HomePage/sections/Hero.tsx
@@ -4,14 +4,15 @@ import PrimaryButton from "../../../components/ui/PrimaryButton";
 const Hero: React.FC = () => {
   return (
     <section className="flex flex-col items-center gap-5 text-center px-5">
-      <h1 className="text-3xl md:text-6xl max-w-3xl font-medium">
+      <h1 className="text-2xl md:text-6xl max-w-3xl font-medium">
         Crafting Ideas into Scalable Web Experiences
       </h1>
-      <p className="max-w-2xl">
+
+      <p className="max-w-2xl text-sm md:text-base text-[var(--color-text-muted)]">
         I’m Mohammed, a software engineer who loves blending design, code, and
         problem-solving to build digital products that solve real-world problems
       </p>
-      {/* TODO: ADD CTA Button Here */}
+
       <PrimaryButton label="Get in Touch" />
     </section>
   );
